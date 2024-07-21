5e5fb3c08514c37cd4d799cf696712157144d0c1
Benned-H <benned1@gmail.com>
2024-07-21 17:08:51 -0400

    Separate out main function

---
+++ main.py
@@ -0,0 +1,27 @@
+"""This script defines the main function for the assignment."""
+
+
+def main():
+    """Define hyperparameters and test either the SARSA or SARSA-λ implementation."""
+
+    game = "Taxi-v3"  # CartPole-v0 or Taxi-v3
+    alpha = 0.2
+    gamma = 0.9
+    epsilon = 0.25
+    lambda_value = 0.9
+    num_episodes = 1500
+    num_runs = 10
+
+    ### Run SARSA Algorithm [uncomment the 3 lines below for testing]
+    # episode_rewards_1, learned_policy = avg_episode_rewards(game, "sarsa", alpha, gamma, epsilon, lambda_value, num_episodes, num_runs)
+    # plot_rewards(episode_rewards_1, game, algorithm="sarsa")
+    # render_visualization(learned_policy, game, model="sarsa", algorithm="sarsa")
+
+    ### Run SARSA-λ Algorithm [uncomment the 3 lines below for testing]
+    # episode_rewards_2, learned_policy = avg_episode_rewards(game, "sarsa_lambda", alpha, gamma, epsilon, lambda_value, num_episodes, num_runs)
+    # plot_rewards(episode_rewards_2, game, algorithm="sarsa_lambda")
+    # render_visualization(learned_policy, game, model="sarsa_lambda", algorithm="sarsa_lambda")
+
+
+if __name__ == "__main__":
+    main()
--- sarsa_algos.py
+++ sarsa_algos.py
@@ -116,22 +116,3 @@ def avg_episode_rewards(
 
     episode_rewards = [er / num_runs for er in episode_rewards]
     return episode_rewards, learned_policy
-
-
-game = "Taxi-v3"  # CartPole-v0 or Taxi-v3
-alpha = 0.2
-gamma = 0.9
-epsilon = 0.25
-lambda_value = 0.9
-num_episodes = 1500
-num_runs = 10
-
-### Run SARSA Algorithm [uncomment the 3 lines below for testing]
-# episode_rewards_1, learned_policy = avg_episode_rewards(game, "sarsa", alpha, gamma, epsilon, lambda_value, num_episodes, num_runs)
-# plot_rewards(episode_rewards_1, game, algorithm="sarsa")
-# render_visualization(learned_policy, game, model="sarsa", algorithm="sarsa")
-
-### Run SARSA-λ Algorithm [uncomment the 3 lines below for testing]
-# episode_rewards_2, learned_policy = avg_episode_rewards(game, "sarsa_lambda", alpha, gamma, epsilon, lambda_value, num_episodes, num_runs)
-# plot_rewards(episode_rewards_2, game, algorithm="sarsa_lambda")
-# render_visualization(learned_policy, game, model="sarsa_lambda", algorithm="sarsa_lambda")
